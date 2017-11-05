b672a9604364148d89ec6c63046e410cba66df7e
Amanda K <kosateam@yahoo.com>
2017-11-05 23:31:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,8 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  return array.push(element)
+  array.push(element)
+  return array
 }
 
 function accessElementInArray(array, index){
@@ -27,7 +28,8 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift()
+  array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array){
